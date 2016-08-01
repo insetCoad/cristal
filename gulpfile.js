2a39447e73d68b1dcb379bc4303fdcea9c66b2fc
insetCoad <insetlive@gmail.com>
2016-08-02 00:01:23 +0530

    gulp- dirList and scss is set

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,29 @@ var gulp = require("gulp"),
     sass = require("gulp-sass"),
     plumber = require("gulp-plumber");
 
+/*| dir list |*/
+var dir = {
+    "src":"./src",
+    "pre":"./pre",
+    "pub":"./pub",
+    "test":"./test"
+}
+var lst = {
+    "jade":"/**/*.jade",
+    "scss":"/**/*.scss",
+    "ts":"/**/*.ts"
+}
+gulp.task('default',['sass']);
 
-gulp.task('default', function() {
-    console.log("i am in");
+/*|scss compiler options |*/
+gulp.task('scss', function() {
+    var scssRes = 
+         gulp.src(dir.src + "/scss" + lst.scss)
+         .pipe(sourceMap.init())
+    return merge([
+        //this is the preview [pre] version..
+        scssRes.pipe(sass().on('error', sass.logError)).pipe(sourceMap.write('./source')).pipe(gulp.dest(dir.pre + "/css")),
+        //their is the main publish [pub] version
+        scssRes.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError)).pipe(gulp.dest(dir.pub + "/css"))
+    ])
 });
