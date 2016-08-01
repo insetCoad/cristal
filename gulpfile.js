1a234cf3ae34b8ac1f186329fb7f0a95f558aea6
insetCoad <insetlive@gmail.com>
2016-08-01 23:59:33 +0530

    npm and lodash looks like ok!!

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require("gulp"),
+    sourceMaps = require("gulp-sourcemaps"),
+    sass = require("gulp-sass"),
+    plumber = require("gulp-plumber");
+
+
+gulp.task('default', function() {
+    console.log("i am in");
+});
